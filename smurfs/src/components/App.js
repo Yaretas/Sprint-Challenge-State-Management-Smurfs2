5f276972396d212de0420f5339ee87aca349c76b
Yara Castro <yaracastro24@gmail.com>
2019-12-02 21:06:19 -0600

    mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,31 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      data:[]
-    }
-  }
 
-  componentDidMount(){
-    fetch('http://localhost:3333/smurfs')
-    .then(res => res.json())
-    .then(smurf => this.setState({ data: smurf}))
-    .catch(err => console.log('hey', err))
-  }
+import SmurfList from "./SmurfList";
+import SmurfForm from "./SmurfForm";
 
-  render() {
-    console.log(this.state);
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <SmurfList />
+      <SmurfForm />
+    </div>
+  );
 }
 
 export default App;
